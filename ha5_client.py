c52baa79fbbde37c9ab1936f5343e1f081aa7c32
rhadov <hannes.niininen@gmail.com>
2020-12-21 22:21:23 +0200

    ver0.2

--- ha5_client.py
+++ ha5_client.py
@@ -6,47 +6,102 @@ import tqdm
 SEPARATOR = "<SEPARATOR>"
 BUF_SIZE = 4096 #Each step buffer size
 
-filename = sys.argv[3]
-filesize = os.path.getsize(filename)
-
 def cmd():
     c = len(sys.argv)
-    if c != 4:
-        print("Error: Not enought parameters \nha3_server.py ip port")
+    if c <= 3:
+        print("Error: Not enought parameters \n'list/send/get/stop' 'ip' 'port' 'file'")
     else:
-        #check the existence of given file
-        if os.path.exists(sys.argv[3]):
-            try:
-                ip = sys.argv[1]
-                port = int(sys.argv[2])
-                main(ip, port)
-            except ValueError:
-                print("ValueError. Numbers only")
-        else:
-            print("File not found")
+        try:
+            global command
+            global ip
+            global port
+            global path
+            command = sys.argv[1]
+            ip = sys.argv[2]
+            port = int(sys.argv[3])
+            path = sys.argv[4]
+            getFunc(command)
+        except IndexError:
+            if command != "send":
+                getFunc(command)
+            else:
+                print("ValueError. No file specified")
+        except ValueError:
+            print("ValueError. Port has to be numbers")
 
-def main(ip, port):
+def getFunc(i):
+    switcher={
+        "list":getList,
+        "send":sendFile,
+        "get":getFile,
+        "stop":stopServer,
+    }
+    func=switcher.get(i,lambda :"Invalid")
+    return func()
+
+
+def stopServer():
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create the socket
     try:
         s.connect((ip, port))
-        s.send(f"{filename}{SEPARATOR}{filesize}".encode())
-        progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-        with open(filename, "rb") as f:
-            for _ in progress:
-                #read the bytes from file
-                bytes_read = f.read(BUF_SIZE)
-                if not bytes_read:
-                    #file transmit done, shutdown informs server not to receive anymore
-                    s.shutdown(socket.SHUT_WR)
-                    break
-                s.sendall(bytes_read)
-                #progress bar update
-                progress.update(len(bytes_read))
-
+        s.send(command.encode())
     except socket.error:
+            print("Cannot send data, something wrong with socket, address or port")
+            print(str(s.recv(1024), "utf-8"))
+    s.close()
+
+def getFile():
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create the socket
+    try:
+        s.connect((ip, port))
+        s.send(command.encode())
+    except socket.error:
+            print("Cannot send data, something wrong with socket, address or port")
+            print(str(s.recv(1024), "utf-8"))
+    s.close()
+
+def getList():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create the socket
+    try:
+        s.connect((ip, port))
+        s.send(command.encode())
+        data = s.recv(BUF_SIZE)
+    except:
         print("Cannot send data, something wrong with socket, address or port")
         print(str(s.recv(1024), "utf-8"))
-    s.close()
+    
+    print("Server files: "+ data.decode())
+
+def sendFile():
+    #check if file exists
+    if os.path.exists(path):
+        global filesize
+        filesize = os.path.getsize(path)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create the socket
+        try:
+            s.connect((ip, port))
+            s.send(f"{command}{SEPARATOR}{path}{SEPARATOR}{filesize}".encode())
+            progress = tqdm.tqdm(range(filesize), f"Sending {path}", unit="B", unit_scale=True, unit_divisor=1024)
+            with open(path, "rb") as f:
+                for _ in progress:
+                    #read the bytes from file
+                    bytes_read = f.read(BUF_SIZE)
+                    if not bytes_read:
+                        #file transmit done, shutdown informs server not to receive anymore
+                        s.shutdown(socket.SHUT_WR)
+                        break
+                    s.sendall(bytes_read)
+                    #progress bar update
+                    progress.update(len(bytes_read))
+
+        except socket.error:
+            print("Cannot send data, something wrong with socket, address or port")
+            print(str(s.recv(1024), "utf-8"))
+        s.close()
+    else:
+        print("File not found")
 
 if __name__ == "__main__":
     cmd()
--- ha5_server.py
+++ ha5_server.py
@@ -12,16 +12,31 @@ def cmd():
     if c != 4:
         print("Error: Not enought parameters \nha3_server.py ip port")
     else:
-        #check if the given path exists
-        if os.path.isdir(sys.argv[3]):
-            #Start the sockserver
-            try:
-                ip = sys.argv[1]
-                port = int(sys.argv[2])
-                sockserv(ip, port)
-            except ValueError:
-                print("ValueError. Numbers only")
-        else: print("Selected path not valid or does not exist")
+        #set given arguments
+        global path
+        global ip
+        global port
+        path = sys.argv[3]
+        ip = sys.argv[1]
+        port = int(sys.argv[2])
+        #create folder
+        try:
+            os.mkdir(path)
+        except OSError:
+            #folder already exist
+            print("Failed to create folder, using existing one")
+            listener()
+        else:
+            #folder created
+            print(f"Created folder {path}")
+            listener()
+
+def listener():
+    #Tries to start listener with given arquments
+    try:
+        sockserv(ip, port)
+    except ValueError:
+        print("ValueError. Numbers only")
 
 def sockserv(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create socket
@@ -35,24 +50,48 @@ def sockserv(ip, port):
 
                 (conn, addr) = s.accept()
                 print(f"{addr} is connected.")
-            
+                
+                #check if command is sent correctly
+                #everything ok
+                                    
                 #receive the file info first
                 receive = conn.recv(BUF_SIZE).decode()
-                (filename, filesize) = receive.split(SEPARATOR)
-                filename = os.path.basename(filename)
-                filesize = int(filesize)
+                arguments = receive.split(SEPARATOR)
+                if arguments[0] == "send":
+                    
+                    #receive the file info first
+                    filename = arguments[1]
+                    filesize = arguments[2]
+                    filename = os.path.basename(filename)
+                    filesize = int(filesize)
 
-                #receive the actual file in 4096 byte chunks
-                progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-                with open(os.path.join(sys.argv[3], filename), "wb") as f:
-                    for _ in progress:
-                        bytes_read = conn.recv(BUF_SIZE)
-                        f.write(bytes_read)
-                        progress.update(len(bytes_read))
-                f.close()
+                    #receive the actual file in 4096 byte chunks
+                    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+                    with open(os.path.join(path, filename), "wb") as f:
+                        for _ in progress:
+                            bytes_read = conn.recv(BUF_SIZE)
+                            f.write(bytes_read)
+                            progress.update(len(bytes_read))
+                        f.close()
+                
+                elif arguments[0] == "list":
+                    data = ""
+                    files = os.listdir(path)
+                    for x in files:
+                        data = data+x+" "
+                    print(data)
+                    conn.send(data.encode())
 
+                
+                elif arguments[0] == "get":
+                    print("GETTING FILE")
+                
+                elif arguments[0] == "stop":
+                    print(f"SERVER TERMINATED BY {addr}")
+                    break
+        
         except socket.error:
             print("Cannot bind port")
-        
+
 if __name__ == "__main__":
     cmd()
